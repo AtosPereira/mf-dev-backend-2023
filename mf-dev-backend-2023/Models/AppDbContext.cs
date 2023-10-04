30ae66de700128ad23e8266eb3a8cc369ae3df6c
AtosPereira <atostom2@gmail.com>
2023-10-04 01:35:22 -0300

    Configuração do Banco de Dados e da Tabela veículo

---
+++ mf-dev-backend-2023/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace mf_dev_backend_2023.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos {  get; set; }  
+    }
+}
